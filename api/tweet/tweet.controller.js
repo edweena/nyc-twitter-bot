fe07c6a391f843fa55bd01a1e6ce410205ed7c98
James Pants <jamespants@gmail.com>
2015-05-17 00:33:20 +0200

    sleep my precious

--- api/tweet/tweet.controller.js
+++ api/tweet/tweet.controller.js
@@ -29,100 +29,100 @@ var Bot = require('../../lib/bot');
 // Twit Init
 // 
 // -------------------------------------------------
-var T = new Twit(config.twitter);
-var bot = new Bot(config.twitter);
+// var T = new Twit(config.twitter);
+// var bot = new Bot(config.twitter);
 
 
-// -------------------------------------------------
-//
-// Listen on tweets
-// 
-// -------------------------------------------------
+// // -------------------------------------------------
+// //
+// // Listen on tweets
+// // 
+// // -------------------------------------------------
 
-var stream = T.stream('statuses/filter', {track: '#stormrave'});
-
-
-// ------------------------------------------------
-// On stream, save to DB
-//
-
-stream.on('tweet', function(tweet){
+// var stream = T.stream('statuses/filter', {track: '#stormrave'});
 
 
+// // ------------------------------------------------
+// // On stream, save to DB
+// //
 
-	// ------------------------------------------------
-	// After response
-	//
+// stream.on('tweet', function(tweet){
+
+
+
+// 	// ------------------------------------------------
+// 	// After response
+// 	//
 	
-	function callback(){
-		console.log('Finished');
-	}
+// 	function callback(){
+// 		console.log('Finished');
+// 	}
 		
-	// ------------------------------------------------
-	// Setup object to save
-	//
+// 	// ------------------------------------------------
+// 	// Setup object to save
+// 	//
 	
-	var tweetObject = {
-		text: tweet.text,
-		username: tweet.user.screen_name,
-		name: tweet.user.name,
-		tweetId: tweet.id_str,
-		image: tweet.user.profile_image_url
-	};
+// 	var tweetObject = {
+// 		text: tweet.text,
+// 		username: tweet.user.screen_name,
+// 		name: tweet.user.name,
+// 		tweetId: tweet.id_str,
+// 		image: tweet.user.profile_image_url
+// 	};
 
 
-	var tweetEntry = new Tweet(tweetObject);
+// 	var tweetEntry = new Tweet(tweetObject);
 
 
-	tweetEntry.save(function(err){
-		if (err){
-			console.log(err);
-		}
-		else{
-			console.log('Tweet saved');
+// 	tweetEntry.save(function(err){
+// 		if (err){
+// 			console.log(err);
+// 		}
+// 		else{
+// 			console.log('Tweet saved');
 
 
-			//get response item
+// 			//get response item
 
-			Response.find({}, function(err, responses){
-				if (err){
-					console.log(err);
-				}
+// 			Response.find({}, function(err, responses){
+// 				if (err){
+// 					console.log(err);
+// 				}
 
-				var randomResponse = responses[Math.floor(Math.random() * responses.length)].text;
+// 				var randomResponse = responses[Math.floor(Math.random() * responses.length)].text;
 				
-				setTimeout(function(){
-					bot.tweet(tweetObject.username, randomResponse, tweetObject.id, callback);
-				},5000);
+// 				setTimeout(function(){
+// 					bot.tweet(tweetObject.username, randomResponse, tweetObject.id, callback);
+// 				},5000);
 
 
-			});
-		}
-	});
-});
+// 			});
+// 		}
+// 	});
+// });
 
 
 	
 
 
-// -------------------------------------------------
-//
-// Handle errors
-// 
-// -------------------------------------------------
+// // -------------------------------------------------
+// //
+// // Handle errors
+// // 
+// // -------------------------------------------------
 
 
-stream.on('limit', function(limitMessage){
-	console.log(limitMessage);
-});
+// stream.on('limit', function(limitMessage){
+// 	console.log(limitMessage);
+// });
 
-stream.on('disconnect', function(disconnectMessage){
-	console.log(disconnectMessage);
-});
+// stream.on('disconnect', function(disconnectMessage){
+// 	console.log(disconnectMessage);
+// });
 
-stream.on('warning', function(warning){
-	console.log(warning);
-});
+// stream.on('warning', function(warning){
+// 	console.log(warning);
+// });
 
 
 
